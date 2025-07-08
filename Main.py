751df2c980622e04b0fbcfbf3ef504d1d979c071
Aleks <alekssandras.staponas@gmail.com>
2025-07-08 10:29:54 +0100

    Added server updating

--- Main.py
+++ Main.py
@@ -1,31 +1,35 @@
 import tkinter as tk
 from tkinter import messagebox
+import base64
+import socket
 
 numberOfClicks = 0
 label = None  # define label at module level to avoid scope issues
 
-# the use of subroutines is for code reusability and organisation
+# Getting IP
+hostname = socket.gethostname()
+IPAddr = socket.gethostbyname(hostname)
+
 def update_image():
     global numberOfClicks, label
     # Makes it accessible to other subroutines
     numberOfClicks += 1
-
     label.config(text=f"The button was clicked {numberOfClicks} times...")
 
+
 def create_imageWindow():
-    # creation of the main window
+
     image = tk.Toplevel()  
     image.title("Cookie Clicker")
     image.geometry("500x500")
     # the title of the window
-    lbl = tk.Label(image, text="Welcome to Archangel + Aleks' Cookie Clicker! ")  
+    lbl = tk.Label(image, text="Welcome to the Cookie Clicker! ")
     lbl.pack(pady=10)  # ensures the text is visible
     return image, lbl
 
 
 def ModuxCookie_Creation(image_window, lbl):
-    cookie_image = tk.PhotoImage(file="/PycharmProjects/PythonProject4/Jeff3.png")
-
+    cookie_image = tk.PhotoImage(file="/home/bob/PycharmProjects/PythonProject/images/BlobAnimations/Jeff3.png")
     # create a button with the cookie image
     button = tk.Button(image_window, image=cookie_image, command=update_image)
     button.image = cookie_image
@@ -41,14 +45,33 @@ def main():
 
 #  LOGIN SECTION
 def check_login():
+    global username
     username = username_entry.get()
     password = password_entry.get()
 
-    if username == "Archangel" and password == "Aleks":
+    if username == "user" and password == "pass":
         login_window.destroy()
         main()  # launch cookie clicker after login
+        send_update()
     else:
         messagebox.showerror("Login Failed", "Incorrect username or password.")
+
+def send_update():
+    global numberOfClicks, username,hostname,IpAddr
+    data = f"{username},{numberOfClicks},{hostname},{IPAddr},\n"
+
+    with open("UpdateLeaderboard.txt", "w") as f:
+        f.write(data)
+
+    s = socket.socket()
+    try:
+        s.connect(("127.0.0.1", 12345))
+        s.send(base64.b64encode(data.encode()))
+    except:
+        pass
+    s.close()
+
+    label.after(10000, send_update)
 
 # create login window
 login_window = tk.Tk()
@@ -63,5 +86,6 @@ password_entry = tk.Entry(login_window, show="*")
 password_entry.grid(row=1, column=1, padx=10, pady=10)
 
 tk.Button(login_window, text="Login", command=check_login).grid(row=2, column=0, columnspan=2, pady=10)
-
 login_window.mainloop()
+
+send_update()
